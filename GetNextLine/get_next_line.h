5785a3d69a30e871d2be12b3f57dcca7af7dc18b
prambaud <philippe.rambaud5@gmail.com>
2025-06-20 11:30:48 +0200

    transfer 42 to git

---
+++ GetNextLine/get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prambaud <prambaud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 13:00:07 by vincent           #+#    #+#             */
+/*   Updated: 2024/11/28 15:24:59 by prambaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 160
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+char	*ft_liberator(char *s1, char *s2, char *s3);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_endoflinecheck(char *buf);
+char	*ft_takeright(char *str);
+char	*ft_takeleft(char *stash);
+int		ft_read_line(int fd, char **str);
+char	*get_next_line(int fd);
+
+#endif
